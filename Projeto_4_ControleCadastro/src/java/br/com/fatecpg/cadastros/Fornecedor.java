8da8e48f48438193fffcca8edc89fa9b0005b965
Felipe Bruno <>
2017-04-28 18:35:18 -0300

    Classe Fornecedor adicionado

---
+++ Projeto_4_ControleCadastro/src/java/br/com/fatecpg/cadastros/Fornecedor.java
@@ -0,0 +1,60 @@
+package br.com.fatecpg.cadastros;
+
+
+
+public class Fornecedor {
+    private String nome;
+    private String telefone;
+    private String email;
+    private String razao;
+    private String cnpj;
+    private String endereco;
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+    public String getRazao() {
+        return razao;
+    }
+
+    public void setRazao(String razao) {
+        this.razao = razao;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+}
